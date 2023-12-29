c9f8cfbdba905fd4e613ab6b951bf6a83c7c11ad
justindpk <jdapatrick@gmail.com>
2023-12-28 19:05:11 -0800

    script changes and hyprland config

--- eww/.config/eww/bar/scripts/radio.sh
+++ eww/.config/eww/bar/scripts/radio.sh
@@ -9,8 +9,11 @@ get_playback_status() {
 max_title_length=20
 max_artist_length=12
 
-# Modified scroll_song_info function with artist handling
-# Modified scroll_song_info function for rightward scrolling
+is_audio_muted() {
+  pactl list sinks | grep -q 'Mute: yes'
+  echo $?
+}
+
 scroll_song_info() {
   local scroll_title="$1"
   local scroll_artist="$2"
@@ -19,31 +22,44 @@ scroll_song_info() {
   
   while true; do
     local status=$(get_playback_status)
-    if [ "$status" = "Playing" ]; then
-      local title_length=${#scroll_title}
-      local artist_length=${#scroll_artist}
+    while [ "$status" = "Paused" ]; do
+      sleep 1
+      status=$(get_playback_status)
+    done
+    local title_length=${#scroll_title}
+    local artist_length=${#scroll_artist}
 
-      # Scroll title to the right if necessary
-      if [ $title_length -gt $max_title_length ]; then
-        title_scroll="${scroll_title: -1:1}${scroll_title:0:$max_title_length-1}"
-        scroll_title="${scroll_title: -1:1}${scroll_title:0:$((title_length - 1))}"
-      else
-        title_scroll="$scroll_title"
-      fi
+    # Scroll title to the right if necessary
+    if [ $title_length -gt $max_title_length ]; then
+      title_scroll="${scroll_title: -1:1}${scroll_title:0:$max_title_length-1}"
+      scroll_title="${scroll_title: -1:1}${scroll_title:0:$((title_length - 1))}"
+    else
+      title_scroll="$scroll_title"
+    fi
 
-      # Scroll artist to the right if necessary
-      if [ $artist_length -gt $max_artist_length ]; then
-        artist_scroll="${scroll_artist: -1:1}${scroll_artist:0:$max_artist_length-1}"
-        scroll_artist="${scroll_artist: -1:1}${scroll_artist:0:$((artist_length - 1))}"
-      else
-        artist_scroll="$scroll_artist"
-      fi
+    # Scroll artist to the right if necessary
+    if [ $artist_length -gt $max_artist_length ]; then
+      artist_scroll="${scroll_artist: -1:1}${scroll_artist:0:$max_artist_length-1}"
+      scroll_artist="${scroll_artist: -1:1}${scroll_artist:0:$((artist_length - 1))}"
+    else
+      artist_scroll="$scroll_artist"
+    fi
 
-      song_info="$title_scroll✦ $artist_scroll"
-      echo " $song_info"
+    song_info="$title_scroll✦ $artist_scroll"
+
+    local mute_status=$(is_audio_muted)
+    if [ $mute_status -eq 0 ]; then
+      # Display mute symbol only
+      echo " $song_info"
       eww update player_widget="$song_info"
+    else
+      # Display song information based on play status
+      if [ "$status" = "Playing" ]; then
+        echo " $song_info"
+        eww update player_widget="$song_info"
       elif [ "$status" = "Paused" ]; then
-      echo " $song_info"
+        echo " $song_info"
+      fi
     fi
 
     sleep 0.48
@@ -81,8 +97,32 @@ onSongChange() {
   scroll_pid=$!
 }
 
-# Poll for initial song value
-initial_song=$(playerctl metadata --format '{{ title }} - {{ artist }}')
+# Function to check if song metadata is valid
+is_valid_metadata() {
+  local metadata="$1"
+  [[ "$metadata" != " - " ]] && return 0 || return 1
+}
+
+# Function to poll for initial song value with retry
+poll_initial_song() {
+  local retries=5
+  local delay=1
+  local metadata
+
+  for (( i=0; i<retries; i++ )); do
+    metadata=$(playerctl metadata --format '{{ title }} - {{ artist }}')
+    if is_valid_metadata "$metadata"; then
+      echo "$metadata"
+      return
+    fi
+    sleep "$delay"
+  done
+
+  echo " - "
+}
+
+# Poll for initial song value with retry mechanism
+initial_song=$(poll_initial_song)
 onSongChange "$initial_song"
 
 playerctl --follow metadata --format '{{ title }} - {{ artist }}' | while read line; do
--- eww/.config/eww/bar/scripts/wifi_drop.sh
+++ eww/.config/eww/bar/scripts/wifi_drop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# kitty @ --to=unix:/run/user/1000/kitty.socket send-text --match=title:list "rofi -show wifi -modi wifi:iwdrofimenu\n"
 kitty --title "WifiConfig" --hold -e sh -c 'rofi -show wifi -modi wifi:iwdrofimenu'
 
